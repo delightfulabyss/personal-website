3741ab819c26ed5b3a91a814db6bf553a1180944
Matthew Watman <mattwatman@gmail.com>
2022-07-21 15:39:41 -0400

    Escape characters

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import { Content } from '../content/Content';
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 
 const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+  <Main
+    meta={
+      <Meta
+        title="About Me"
+        description="Description of who I am and what I do"
+      />
+    }
+  >
     <Content>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
-        labore voluptatibus distinctio recusandae autem esse explicabo molestias
-        officia placeat, accusamus aut saepe.
+        Hi there! My name is Matthew or you can call me delightfulabyss. I&apos;
+        a full-stack web3 developer who specializes in writing decentralized
+        applications using the Ethereum blockchain.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
-        labore voluptatibus distinctio recusandae autem esse explicabo molestias
-        officia placeat, accusamus aut saepe.
+        Most of the time you can find me helping to build a decentralized city
+        with <Link href="https://www.creatorcabins.com">Cabin DAO</Link> and
+        learning and earning with the{' '}
+        <Link href="https://www.solidityguild.com">Solidity Guild</Link>. When
+        I&apos;m not doing that, I enjoy exploratory travel, photography,
+        Dungeons and Dragons and making music.
       </p>
     </Content>
   </Main>
